883bfd8044a497225118c4898c64aa4202f1c5b4
Brian C Sparks <briancsparks@gmail.com>
2023-09-16 16:52:00 -0700

    Refactoring

--- main.go
+++ main.go
@@ -31,6 +31,88 @@ func readFifo(line chan string) {
 	}
 }
 
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	screen, err := tcell.NewScreen()
+	if err != nil {
+		panic(err)
+	}
+	if err := screen.Init(); err != nil {
+		panic(err)
+	}
+	defer screen.Fini()
+
+	screen.Clear()
+
+	f, err := os.OpenFile("/tmp/termgridboard", os.O_RDONLY, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	quit := make(chan bool)
+	line := make(chan string, 100)
+
+    go readFifo(line)
+
+    problemJson := ""
+
+	go func() {
+		for {
+			select {
+			case <-quit:
+				return
+
+			case l := <-line:
+				var cell Cell
+				if err := json.Unmarshal([]byte(l), &cell); err != nil {
+                    problemJson = l
+					continue // Skip invalid JSON
+				}
+
+				if cell.X >= 0 && cell.Y >= 0 {
+                    style := tcell.StyleDefault
+
+                    color, err := colorCode(cell.Color)
+                    if err == nil {
+                        style = style.Foreground(color)
+                    }
+
+					screen.SetCell(cell.X, cell.Y, style, rune(cell.Sym))
+				}
+
+				if cell.Redraw {
+					screen.Show()
+				}
+			}
+		}
+	}()
+
+	// Block, waiting for input from the user
+	for {
+		ev := screen.PollEvent()
+		switch ev := ev.(type) {
+		case *tcell.EventKey:
+			if ev.Key() == tcell.KeyEscape || ev.Rune() == 'q' {
+                fmt.Print("Problem JSON: ")
+                fmt.Println(problemJson)
+				close(quit)
+				return
+			}
+            if ev.Key() == tcell.KeyCtrlL {
+                screen.Sync()
+            }
+        case *tcell.EventResize:
+            screen.Sync()
+		}
+	}
+
+}
+
+
+
 func colorCode(name string) (tcell.Color, error) {
     if name == "red" {
         return tcell.ColorRed, nil
@@ -75,93 +157,3 @@ func colorCode(name string) (tcell.Color, error) {
 // ColorFuchsia
 // ColorAqua
 // ColorWhite
-
-
-func main() {
-	fmt.Println("Hello, World!")
-
-	screen, err := tcell.NewScreen()
-	if err != nil {
-		panic(err)
-	}
-	if err := screen.Init(); err != nil {
-		panic(err)
-	}
-	defer screen.Fini()
-
-	screen.Clear()
-
-	f, err := os.OpenFile("/tmp/termgridboard", os.O_RDONLY, 0600)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	//scanner := bufio.NewScanner(f)
-
-	quit := make(chan bool)
-	line := make(chan string, 100)
-
-    go readFifo(line)
-
-	//// Goroutine to read from the FIFO
-	//go func() {
-	//	for scanner.Scan() {
-	//		line <- scanner.Text()
-	//	}
-	//	//fmt.Println("Scanner done.")
-	//}()
-
-    problem_json := ""
-
-	go func() {
-		for {
-			select {
-			case <-quit:
-				return
-
-			case l := <-line:
-				var cell Cell
-				if err := json.Unmarshal([]byte(l), &cell); err != nil {
-                    problem_json = l
-					continue // Skip invalid JSON
-				}
-
-				if cell.X >= 0 && cell.Y >= 0 {
-                    style := tcell.StyleDefault
-
-                    color, err := colorCode(cell.Color)
-                    if err == nil {
-                        style = style.Foreground(color)
-                    }
-
-					screen.SetCell(cell.X, cell.Y, style, rune(cell.Sym))
-				}
-
-				if cell.Redraw {
-					screen.Show()
-				}
-			}
-		}
-	}()
-
-	// Block, waiting for input from the user
-	for {
-		ev := screen.PollEvent()
-		switch ev := ev.(type) {
-		case *tcell.EventKey:
-			if ev.Key() == tcell.KeyEscape || ev.Rune() == 'q' {
-                fmt.Print("Problem JSON: ")
-                fmt.Println(problem_json)
-				close(quit)
-				return
-			}
-            if ev.Key() == tcell.KeyCtrlL {
-                screen.Sync()
-            }
-        case *tcell.EventResize:
-            screen.Sync()
-		}
-	}
-
-}
